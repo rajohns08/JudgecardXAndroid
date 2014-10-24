dc5cb0ee9c74f4c1a07b4e43d94512098c6bbbd5
rajohns <robert.adam.johns@gmail.com>
2014-10-23 22:33:34 -0500

    adding butterknife

--- app/src/main/java/com/rajohns/judgecardx/LoginActivity.java
+++ app/src/main/java/com/rajohns/judgecardx/LoginActivity.java
@@ -7,6 +7,9 @@ import android.widget.EditText;
 
 import javax.inject.Inject;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -16,44 +19,37 @@ import static com.rajohns.judgecardx.NotifyHelper.*;
 
 public class LoginActivity extends BaseActivity {
     @Inject RestClient restClient;
+    @InjectView(R.id.usernameET) EditText usernameET;
+    @InjectView(R.id.passwordET) EditText passwordET;
 
-    public EditText usernameET;
-    public EditText passwordET;
+    @OnClick(R.id.signInButton) void signIn() {
+        NotifyHelper.showLoading(LoginActivity.this);
+        restClient.login(usernameET.getText().toString(), passwordET.getText().toString(), new Callback<String>() {
+            @Override
+            public void success(String responseString, Response response) {
+                NotifyHelper.hideLoading();
+                if (responseString.equals(LOGIN_SUCCESS)) {
+                }
+                else if (responseString.equals(LOGIN_FAILURE)) {
+                    NotifyHelper.showSingleButtonAlert(LoginActivity.this, BAD_CREDENTIALS_TITLE, BAD_CREDENTIALS_MSG);
+                }
+                else {
+                    NotifyHelper.showSingleButtonAlert(LoginActivity.this, GENERIC_ERROR_TITLE, GENERIC_ERROR_MSG);
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError retrofitError) {
+                NotifyHelper.hideLoading();
+                NotifyHelper.showSingleButtonAlert(LoginActivity.this, GENERIC_ERROR_TITLE, GENERIC_ERROR_MSG);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        usernameET = (EditText)findViewById(R.id.usernameET);
-        passwordET = (EditText)findViewById(R.id.passwordET);
-
-        Button signInButton = (Button)findViewById(R.id.signInButton);
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NotifyHelper.showLoading(LoginActivity.this);
-                restClient.login(usernameET.getText().toString(), passwordET.getText().toString(), new Callback<String>() {
-                    @Override
-                    public void success(String responseString, Response response) {
-                        NotifyHelper.hideLoading();
-                        if (responseString.equals(LOGIN_SUCCESS)) {
-                        }
-                        else if (responseString.equals(LOGIN_FAILURE)) {
-                            NotifyHelper.showSingleButtonAlert(LoginActivity.this, BAD_CREDENTIALS_TITLE, BAD_CREDENTIALS_MSG);
-                        }
-                        else {
-                            NotifyHelper.showSingleButtonAlert(LoginActivity.this, GENERIC_ERROR_TITLE, GENERIC_ERROR_MSG);
-                        }
-                    }
-
-                    @Override
-                    public void failure(RetrofitError retrofitError) {
-                        NotifyHelper.hideLoading();
-                        NotifyHelper.showSingleButtonAlert(LoginActivity.this, GENERIC_ERROR_TITLE, GENERIC_ERROR_MSG);
-                    }
-                });
-            }
-        });
+        ButterKnife.inject(this);
     }
 }
