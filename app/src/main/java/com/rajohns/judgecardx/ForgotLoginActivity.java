65e96f7e71a8e9595eccea403763a2b8cffc9ebb
rajohns <robert.adam.johns@gmail.com>
2014-11-09 20:23:23 -0600

    adding some forgot login functionality

--- app/src/main/java/com/rajohns/judgecardx/ForgotLoginActivity.java
+++ app/src/main/java/com/rajohns/judgecardx/ForgotLoginActivity.java
@@ -4,18 +4,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.InjectViews;
 import butterknife.OnClick;
 import butterknife.OnFocusChange;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+import static com.rajohns.judgecardx.RestClient.*;
+import static com.rajohns.judgecardx.NotifyHelper.*;
 
 /**
  * Created by rajohns on 10/25/14.
  *
  */
 public class ForgotLoginActivity extends BaseActivity {
-    ArrayList<EditText> requiredEditTexts;
+    @Inject RestClient restClient;
+    @InjectView(R.id.emailET) EditText emailET;
+
+    @InjectViews({R.id.emailET})
+    List<EditText> requiredEditTexts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,33 @@ public class ForgotLoginActivity extends BaseActivity {
     }
 
     @OnClick(R.id.emailMeButton) void emailMe() {
+        if (EditTextUtil.hasEmptyRequiredTextField(requiredEditTexts)) {
+            NotifyHelper.showSingleButtonAlert(this, MISSING_EMAIL_FORGOT_TITLE, MISSING_EMAIL_FORGOT_MSG);
+            return;
+        }
+
+        // service call
+        NotifyHelper.showLoading(this);
+        restClient.sendEmail(emailET.getText().toString(), new Callback<String>() {
+            @Override
+            public void success(String responseString, Response response) {
+                NotifyHelper.hideLoading();
+                if (responseString.equals(FORGOT_LOGIN_SUCCESS)) {
+                    NotifyHelper.showSingleButtonAlert(ForgotLoginActivity.this, EMAIL_SENT_TITLE, EMAIL_SENT_MSG);
+                }
+                else if (responseString.equals(EMAIL_NOT_FOUND)) {
+                    NotifyHelper.showSingleButtonAlert(ForgotLoginActivity.this, getResources().getString(R.string.emailNotFoundTitle), getResources().getString(R.string.emailNotFoundMsg));
+                }
+                else {
+                    NotifyHelper.showSingleButtonAlert(ForgotLoginActivity.this, getResources().getString(R.string.unknownErrorTitle), getResources().getString(R.string.unknownErrorMsg));
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                NotifyHelper.hideLoading();
+            }
+        });
 
     }
 
--- app/src/main/java/com/rajohns/judgecardx/RestClient.java
+++ app/src/main/java/com/rajohns/judgecardx/RestClient.java
@@ -17,8 +17,14 @@ public interface RestClient {
     public static final String BASE_URL = "https://secure3017.hostgator.com/~rajohns/";
     public static final String LOGIN_SUCCESS = "com.judgecard.successfulLogin";
     public static final String LOGIN_FAILURE = "com.judgecard.badCredentials";
+    public static final String FORGOT_LOGIN_SUCCESS = "com.judgecard.sendingEmail";
+    public static final String EMAIL_NOT_FOUND = "com.judgecard.userNotFound";
 
     @FormUrlEncoded
     @POST("/login.php")
     void login(@Field("username") String username, @Field("password") String password, Callback<String> callback);
+
+    @FormUrlEncoded
+    @POST("/forgotLoginSSL.php")
+    void sendEmail(@Field("email") String email, Callback<String> callback);
 }
