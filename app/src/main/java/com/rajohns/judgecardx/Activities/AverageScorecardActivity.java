d4fdfe417a7e1dfe6357fec29080f940aae6a82d
rajohns <robert.adam.johns@gmail.com>
2015-02-14 21:29:48 -0600

    adding avg scorecard activity

---
+++ app/src/main/java/com/rajohns/judgecardx/Activities/AverageScorecardActivity.java
@@ -0,0 +1,14 @@
+package com.rajohns.judgecardx.Activities;
+
+import android.os.Bundle;
+
+/**
+ * Created by rajohns on 2/14/15.
+ *
+ */
+public class AverageScorecardActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
