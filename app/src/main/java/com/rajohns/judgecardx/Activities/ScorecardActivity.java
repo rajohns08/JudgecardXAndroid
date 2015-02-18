3b6e5f484c87c6e2258c7af02162544d181982a7
rajohns <robert.adam.johns@gmail.com>
2015-02-17 20:02:04 -0600

    correctly handling options menu clicks

--- app/src/main/java/com/rajohns/judgecardx/Activities/ScorecardActivity.java
+++ app/src/main/java/com/rajohns/judgecardx/Activities/ScorecardActivity.java
@@ -309,8 +309,19 @@ public class ScorecardActivity extends BaseActivity {
         return true;
     }
 
-    public boolean avgScorecardClicked(MenuItem item) {
-        startActivity(new Intent(this, AverageScorecardActivity.class));
-        return true;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.avg_scorecard) {
+            startActivity(new Intent(this, AverageScorecardActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
