b1291ddcbb61e99aa80f4a46c7b7a2b617d2a42e
rajohns <robert.adam.johns@gmail.com>
2015-03-03 00:01:44 -0600

    adding judge number action bar item clickability

--- app/src/main/java/com/rajohns/judgecardx/Activities/AverageScorecardActivity.java
+++ app/src/main/java/com/rajohns/judgecardx/Activities/AverageScorecardActivity.java
@@ -192,4 +192,23 @@ public class AverageScorecardActivity extends BaseActivity {
         numPeopleMenuItem = menu.findItem(R.id.num_people);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.num_people) {
+            String person = this.getResources().getQuantityString(R.plurals.person, numOfCards);
+            String is = this.getResources().getQuantityString(R.plurals.is, numOfCards);
+            String has = this.getResources().getQuantityString(R.plurals.has, numOfCards);
+            NotifyHelper.showSingleButtonAlert(this, "Number of judges", numOfCards + " " + person + " " + has + " scored or " + is + " scoring this fight.");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
