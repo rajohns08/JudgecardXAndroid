c4f26f710d815b2995806b16f2f817d993018a4d
rajohns <robert.adam.johns@gmail.com>
2015-04-26 11:32:16 -0500

    taking out unnecessary import

--- app/src/main/java/com/rajohns/judgecardx/Activities/LoginActivity.java
+++ app/src/main/java/com/rajohns/judgecardx/Activities/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.rajohns.judgecardx.Activities;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
